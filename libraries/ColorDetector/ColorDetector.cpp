0641acfa3a0f88c5e71bd821646b40e1006cdcfc
Justin Johnson <johnsonj561@gmail.com>
2016-05-25 21:49:57 -0400

    detectColor() updated

--- libraries/ColorDetector/ColorDetector.cpp
+++ libraries/ColorDetector/ColorDetector.cpp
@@ -1,21 +1,32 @@
-/*
-  ColorDetector.cpp - Library for detecting white and black objects.
-  Justin Johnson Summer 2016
-  Summer 2016
+/**
+*ColorDetector.cpp - Library for detecting white and black objects.
+*Justin Johnson Summer 2016
+*Summer 2016
 */
 
 #include "Arduino.h"
 #include "ColorDetector.h"
+#include "LED.h"
 
-ColorDetector::ColorDetector(int ledPin, int photoresistorPin){
-  pinMode(ledPin, OUTPUT);
-  pinMode(photoresistorPin, INPUT);
-  _ledPin = ledPin;
+/**
+*Constructor declares LED with aruino output pin of ledPin and 
+*configures analog input pin to read photoresistor values
+*/
+ColorDetector::ColorDetector(int ledPin, int photoresistorPin): _led(ledPin){
   _photoresistorPin = photoresistorPin;
+  _ledPin = ledPin;
+  pinMode(_photoresistorPin, INPUT);
 }
 
+/**
+*Flashes LED and calculates value of light reflected back onto photoresistor
+*@return value of potoresistor detected by arduino
+*/
 int ColorDetector::detectColor(){
-  digitalWrite(_ledPin, HIGH);
-  delay(500);
-  return analogRead(_photoresistorPin);
+  _led.turnOnLED();
+  delay(100);
+  int colorValue = analogRead(_photoresistorPin);
+  delay(100);
+  _led.turnOffLED();
+  return colorValue;
 }
--- libraries/ColorDetector/ColorDetector.h
+++ libraries/ColorDetector/ColorDetector.h
@@ -1,20 +1,22 @@
-/*
-  ColorDetector.h Library for detecting white and black objects.
-  Justin Johnson Summer 2016
-  ED1
+/**
+*ColorDetector.h Library for detecting white and black objects.
+*Justin Johnson Summer 2016
+*ED1 Team 5
 */
 #ifndef ColorDetector_h
 #define ColorDetector_h
 
 #include "Arduino.h"
+#include "LED.h"
 
 class ColorDetector{
   public:
     ColorDetector(int ledPin, int photoresistorPin);
     int detectColor();
   private:
-    int _ledPin;
     int _photoresistorPin;
+    int _ledPin;
+    LED _led;
 };
 
 
